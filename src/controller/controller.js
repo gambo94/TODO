2db127752d72a1afa998dc5b2d50e6bd0485b4de
Eric Garcia <eric.garciad@gmail.com>
2021-04-08 18:38:05 +0200

    inquirer

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,16 +1,65 @@
-const service = require('../services/service');
+const addTask = require('../services/addTask');
+const inquirer = require('inquirer');
 
 const redirect = (answers) => {
     if(answers.command === 'Add task'){
-        service.addTask
+        console.log("¿Qué tarea quieres crear?");
+        var questions = [
+            {
+              type: 'confirm',
+              name: 'toBeDelivered',
+              message: 'Do you want add task?',
+              default: false,
+            },
+            {
+              type: 'input',
+              name: 'title',
+              message: 'Task title?',
+              validate: function (value) {
+              
+                return true 
+              }
+             
+            },
+          ];
+          
+          inquirer.prompt(questions).then((answers) => {
+            console.log('\nTarea crada:');
+            console.log(JSON.stringify(answers, null, '  '));
+            addTask.receriveAnswer(answers);
+          });
+          
     }
-    if(answers.coomand === 'Show task(s)'){
+    if(answers.command === 'Show task(s)'){
 
     }
-    if(answers.coomand === 'Edit task'){
-        
+    if(answers.command === 'Edit task'){
+        console.log("¿Qué tarea quieres editar?");
+        var questions = [
+            {
+              type: 'confirm',
+              name: 'toBeDelivered',
+              message: 'Do you want edit task?',
+              default: false,
+            },
+            {
+              type: 'input',
+              name: 'title',
+              message: 'Task id?',
+              validate: function (value) {
+              
+                return true 
+              }
+             
+            },
+          ];
+          
+          inquirer.prompt(questions).then((answers) => {
+            console.log('\nTarea crada:');
+            console.log(JSON.stringify(answers, null, '  '));
+          });
     }
-    if(answers.coomand === 'Remove task'){
+    if(answers.command === 'Remove task'){
         
     }
 }
