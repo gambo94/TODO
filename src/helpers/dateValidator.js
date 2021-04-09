cfff63f6b201cd696fb1c79bf3e0317c2a2962e2
gambo94 <fedigambo94@gmail.com>
2021-04-09 19:51:41 +0200

    minor changes to date validator, there was a problem with the new Date object format

--- src/helpers/dateValidator.js
+++ src/helpers/dateValidator.js
@@ -5,9 +5,9 @@ let finishDate = {};
 
 // this function validates the start date and stores it in an object so that you can compare it later
 const isValid = (date) =>{
-    let valid = isDate(date, responseType="boolean", dateFormat="dd/mm/yyyy");
+    let valid = isDate(date, responseType="boolean", dateFormat="mm/dd/yyyy");
     if (!valid){
-        return 'Please insert a valid date <dd/mm/yyyy>';
+        return 'Please insert a valid date <mm/dd/yyyy>';
     } else{
         startDate = new Date(date);
         return valid;
@@ -16,11 +16,11 @@ const isValid = (date) =>{
 
 // this function validates the finish date and compares it with the start date to check if user inserted a date before the start date
 const isOlder = (date) => {
-    let valid = isDate(date, responseType="boolean", dateFormat="dd/mm/yyyy");
+    let valid = isDate(date, responseType="boolean", dateFormat="mm/dd/yyyy");
     if(valid === true){
         finishDate = new Date(date);
     } else {
-        return 'Please insert a valid date <dd/mm/yyyy>';
+        return 'Please insert a valid date <mm/dd/yyyy>';
     }
     if(finishDate < startDate){
         return 'Time travel has not been invented yet. Please, insert a date ahead of the start date'
